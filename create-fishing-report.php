4f1edc528e73ab9c4d8beeb09b9e30b7bdb77787
Webmaster444 <jlmobile710@gmail.com>
2020-11-12 23:03:50 +0800

    file error

--- create-fishing-report.php
+++ create-fishing-report.php
@@ -709,8 +709,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         $.ajax({
                             url:'core.php',
                             method:'POST',
-                            processData: false,
-                            contentType: 'application/octet-stream',
                             data:{image:base64data,action:'upload-report-image'},
                             success:function(data)
                             {
