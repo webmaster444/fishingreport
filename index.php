ff0b75b3ad0edf0c34222516ffa087ed1aa3ecc6
Webmaster444 <jlmobile710@gmail.com>
2020-12-05 18:35:13 +0800

    false added

--- index.php
+++ index.php
@@ -30,9 +30,8 @@ $member_role_no = $row[0];
 
 $sql = "SELECT member_type FROM MemberType WHERE member_type_id = ".$member_role_no;
 $member_type_query= $conn->query($sql);
-var_dump($member_type_query);
-$member_role = "";
-if($member_type_query){
+
+if(!$member_type_query){
     if($member_type_query->num_rows==0){
         $row = $member_type_query->fetch_array();
         $member_role = $row[0];
