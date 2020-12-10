af8829c41f7bac2ed6aecf42eac3ae9a23bdd05f
Webmaster444 <jlmobile710@gmail.com>
2020-12-11 00:44:28 +0800

    smtp changed

--- php-email.php
+++ php-email.php
@@ -9,7 +9,7 @@ $mail->Host = 'smtp.gmail.com';
 $mail->Port = '465';
 $mail->isHTML();
 $mail->Username = 'jlmobile710@gmail.com';
-$mail->Password = 'eoqldir111';
+$mail->Password = '#aaaa';
 $mail->SetFrom('your-email-id@gmail.com','Your Name To Be Displayed');
 $mail->Subject = "Your email Subject";
 $mail->Body = 'Any HTML content';
@@ -17,7 +17,7 @@ $mail->Body = 'Any HTML content';
 $mail->AddAddress('jlmobile710@gmail.com');
 
 $result = $mail->Send();
-
+echo $mail->ErrorInfo;
 if($result == 1){
     echo "OK Message";
 }
