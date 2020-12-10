66c01a7aafca7f9c1bf28ac6d746cd5a6c018ddc
Webmaster444 <jlmobile710@gmail.com>
2020-12-11 00:25:27 +0800

    mail test

--- php-email.php
+++ php-email.php
@@ -1,3 +1,27 @@
 <?php
-    mail('jlmobile710@gmail.com','Fishing Report Submission',"One report is created");
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'jlmobile710@gmail.com';
+$mail->Password = 'eoqldir111';
+$mail->SetFrom('your-email-id@gmail.com','Your Name To Be Displayed');
+$mail->Subject = "Your email Subject";
+$mail->Body = 'Any HTML content';
+
+$mail->AddAddress('jlmobile710@gmail.com');
+
+$result = $mail->Send();
+
+if($result == 1){
+    echo "OK Message";
+}
+else{
+    echo "Sorry. Failure Message";
+}
 ?>
