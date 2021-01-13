2e3760aef10f36fff2433f8359443153e969b9ee
Webmaster444 <jlmobile710@gmail.com>
2021-01-13 16:21:29 +0800

    mail test

--- _php-email.php
+++ _php-email.php
@@ -1,6 +1,8 @@
 <?php
 require_once('vendor/PHPMailer/PHPMailerAutoload.php');
-
+require_once "config.php";
+global $mail;
+global $mail_pwd;
 $mail = new PHPMailer();
 $mail->isSMTP();
 $mail->SMTPAuth = true;
@@ -8,8 +10,8 @@ $mail->SMTPSecure = 'tls';
 $mail->Host = 'smtp.gmail.com';
 $mail->Port = '587';
 $mail->isHTML();
-$mail->Username = 'jlmobile710@gmail.com';
-$mail->Password = 'eoqldir111';
+$mail->Username = $mail;
+$mail->Password = $mail_pwd;
 $mail->SetFrom('drupio@gmail.com','Dru Pio');
 $mail->Subject = "Approve new catch log";
 $mail->Body = '<a href="https://dru-pio.myshopify.com/admin/products?selectedView=all&product_type=Angler%20Advisor%20%7C%20Fishing%20Reports%20%7C%20Catch%20Logs&order=created_at%20desc">Approve new catch log</a>';
